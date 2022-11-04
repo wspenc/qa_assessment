d12fe0a91835d48518f1c6d8a56b6e6980296668
Will Spencer <willspencer@me.com>
2022-11-04 12:30:17 -0600

    done

--- functions.test.ts
+++ functions.test.ts
@@ -1,10 +1,10 @@
 const {shuffleArray} = require('./utils')
-
-// describe('shuffleArray should', () => {
-//     test(' return an array', () =>{
-//         expect(typeof shuffleArray(shuffleArray.arrCopy)).toBe('number')
-//     })
-//     test(' shuffle the items', () => {
-//         expect(typeof shuffleArray(shuffleArray.j)).toBe('number')
-//     })
-//   })
+//Come back to study more on testing
+describe('shuffleArray should', () => {
+    test(' return an array', () =>{
+        expect(typeof shuffleArray(shuffleArray.arrCopy)).toBe('number')
+    })
+    test(' shuffle the items', () => {
+        expect(typeof shuffleArray(shuffleArray.j)).toBe('number')
+    })
+  })
